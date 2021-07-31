552c339ab5ab22b4b8a14ed77f3a87b0c2c55ad7
Raytwo <raytwost@gmail.com>
2021-07-31 16:54:23 +0200

    Complete example code, requires debugging

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
+use core::panic;
 use std::ffi::CStr;
-use skyline::{hook, install_hook};
+use skyline::{hook, install_hook, install_hooks};
 
 pub static mut DEVICE_INITIALIZE_OFFS: *const u8 = 0 as _;
 pub static mut QUEUE_SUBMIT_COMMANDS_OFFS: *const u8 = 0 as _;
+pub static mut SET_TEXTURE_OFFS: *const u8 = 0 as _;
+pub static mut ACQUIRE_TEXTURE_OFFS: *const u8 = 0 as _;
 pub static mut DEVICE: *const nvn::Device = 0 as _;
+pub static mut TEXTURES: Vec<*mut nvn::Texture> = Vec::new();
+pub static mut TEXTURE_IDX: i32 = 0;
+
 
 
 extern "C" {
@@ -33,61 +39,99 @@ pub fn bootstrap_loader(func: *const u8) -> *const u8 {
 }
 
 #[hook(replace = DEVICE_INITIALIZE_OFFS)]
-pub extern "C" fn device_initialize(device: *const nvn::Device, device_builder: *const nvn::DeviceBuilder) -> bool {
-    println!("Custom nvnDeviceInitialize called");
-
+pub fn device_initialize(device: *const nvn::Device, device_builder: *const nvn::DeviceBuilder) -> bool {
     unsafe { DEVICE = device };
 
     let result = original!()(device, device_builder);
     
     assert_eq!(result, true, "nvnDeviceInitialize returned false");
+
     unsafe { QUEUE_SUBMIT_COMMANDS_OFFS = nvnBootstrapLoader(skyline::c_str("nvnQueueSubmitCommands\0")) };
-    install_hook!(queue_submit_commands);
+    unsafe { SET_TEXTURE_OFFS = nvnBootstrapLoader(skyline::c_str("nvnWindowBuilderSetTextures\0")) };
+    unsafe { ACQUIRE_TEXTURE_OFFS = nvnBootstrapLoader(skyline::c_str("nvnWindowAcquireTexture\0")) };
+
+
+    install_hooks!(queue_submit_commands, set_textures, acquire_texture);
 
     result
 }
 
-#[hook(replace = QUEUE_SUBMIT_COMMANDS_OFFS)]
-pub extern "C" fn queue_submit_commands(queue: &mut nvn::Queue, count: usize, handles: *mut nvn::CommandHandle) {
-    println!("Custom nvnQueueSubmitCommands called");
-
-    let device = unsafe { &*DEVICE };
-
-    let mut command_handles = unsafe { std::slice::from_raw_parts_mut(handles, count) };
-    let mut test: Vec<nvn::CommandHandle> = vec![];
-    test.extend_from_slice(command_handles);
-
-    // Create a MemoryPoolBuilder for our MemoryPool
-    let mut mem_builder = nvn::MemoryPoolBuilder::new();
-
-    mem_builder.set_defaults();
-    mem_builder.set_device(device);
-    mem_builder.set_flags(nvn::MemoryPoolFlags::new().with_cpu_uncached(true).with_gpu_cached(true));
-    mem_builder.set_storage(unsafe { libc::memalign(0x1000, 0x1000) } as _, 0x1000);
-
-    let mut mem_pool = nvn::MemoryPool::new();
-    mem_pool.initialize(&mem_builder);
-
-    // Create our CommandBuffer
-    let mut comm_buf = nvn::CommandBuffer::new();
-    // TODO: Set Command and Control memory here. 0x1000 in size for both.
-    comm_buf.add_command_memory(&mem_pool, 0, 0x1000);
-    comm_buf.add_control_memory(unsafe { libc::memalign(0x1000, 0x1000) } as _, 0x1000);
-    // Initialize the CommandBuffer now that everything is ready
-    assert_eq!(comm_buf.initialize(device), true, "Couldn't initialize CommandBuffer");
-    // Start recording commands to scissor and clear an area of the screen
-    println!("Begin Recording for CommandBuffer");
-    comm_buf.begin_recording();
-    comm_buf.set_scissor(0, 0, 512, 512);
-    //comm_buf.clear_color(0, 0 as _, o as _);
-    // Stop recording, get your CommandHandle
-    let handle = comm_buf.end_recording();
-    println!("Ended recording, handle acquired: {:#?}", handle);
-
-    // TODO: Append our new CommandHandler to the list
-    println!("Command handles count: {}", count);
+#[hook(replace = SET_TEXTURE_OFFS)]
+pub fn set_textures(builder: *const u8, count: i32, textures: *const *mut nvn::Texture) {
+    if textures != 0 as _ {
+        let texs = unsafe { std::slice::from_raw_parts(textures, count as _) }.to_vec();
+        unsafe { TEXTURES = texs };
+        //dbg!(&texs);
+        //println!("cock: {}", texs.len());
+    }
     
-    original!()(queue, count, handles)
+    original!()(builder, count, textures);
+}
+
+#[hook(replace = ACQUIRE_TEXTURE_OFFS)]
+pub fn acquire_texture(window: *const u8, texture_available: *const u8, texture_idx: *const i32) -> i32 {
+    unsafe { TEXTURE_IDX = *texture_idx };
+    original!()(window, texture_available, texture_idx)
+}
+
+#[hook(replace = QUEUE_SUBMIT_COMMANDS_OFFS)]
+pub fn queue_submit_commands(queue: &nvn::Queue, count: usize, handles: *mut nvn::CommandHandle) {
+    if unsafe { TEXTURES.len() != 0} {
+        let device = unsafe { &*DEVICE };
+        let texture = unsafe { TEXTURES[TEXTURE_IDX as usize] };
+        let render_target: [*const nvn::Texture;1] = [texture];
+
+        // Copy all the handles in a vector for ease of use
+        let mut command_handles = unsafe { std::slice::from_raw_parts_mut(handles, count) };
+        let mut new_handles: Vec<nvn::CommandHandle> = vec![];
+        new_handles.extend_from_slice(command_handles);
+
+        // Create a MemoryPoolBuilder for our MemoryPool
+        let mut mem_builder = nvn::MemoryPoolBuilder::new();
+        let command_mem = unsafe { libc::memalign(0x1000, 0x4000) } as _;
+
+        mem_builder.set_defaults();
+        mem_builder.set_device(device);
+        mem_builder.set_flags(nvn::MemoryPoolFlags::new().with_cpu_uncached(true).with_gpu_cached(true));
+        mem_builder.set_storage(command_mem, 0x4000);
+
+        let mut mem_pool = nvn::MemoryPool::new();
+        assert_eq!(mem_pool.initialize(&mem_builder), true, "Couldn't initialize MemoryPool");
+
+        // Create our CommandBuffer
+        let mut command_buf = nvn::CommandBuffer::new();    
+        // Initialize the CommandBuffer
+        assert_eq!(command_buf.initialize(device), true, "Couldn't initialize CommandBuffer");
+        let control_mem = unsafe { libc::memalign(0x1000, 0x4000) } as _;
+        command_buf.add_command_memory(&mem_pool, 0, 0x4000);
+        command_buf.add_control_memory(control_mem, 0x4000);
+
+        let clear_color: &[f32;4] = &[1.0, 1.0, 0.0, 1.0];
+
+        // Start recording commands to scissor and clear an area of the screen
+        println!("Begin Recording for CommandBuffer");
+        command_buf.begin_recording();
+
+        command_buf.set_render_targets(1, &render_target as _, 0 as _, 0 as _, 0 as _);
+        command_buf.set_scissor(0, 0, 1280, 720);
+        command_buf.set_viewport(0, 0, 1280, 720);
+        command_buf.clear_color(0, clear_color as _, nvn::ClearColorMask::new().with_rgba(true));
+
+        // Stop recording, store our CommandHandle
+        new_handles.push(command_buf.end_recording());
+
+        // Call the original function with our new handle appended
+        original!()(queue, new_handles.len(), new_handles.as_mut_slice().as_mut_ptr() as _);
+
+        // Cleanup
+        command_buf.finalize();
+        //mem_pool.finalize();
+        unsafe { libc::free(command_mem as _) };
+        unsafe { libc::free(control_mem as _) };
+    }
+    else {
+        original!()(queue, count, handles);
+    }
 }
 
 #[skyline::main(name = "nvn_research")]
