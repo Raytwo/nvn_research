1348fd6707b47239945b7b1190d13789cb658492
blu-dev <the.blu.dev@gmail.com>
2021-07-31 19:28:32 -0700

    change nvn-rs and begin adding managed memory objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,45 @@
 use core::panic;
 use std::ffi::CStr;
-use nvn::ClearColorMask;
 use skyline::{hook, install_hook, install_hooks};
+use lazy_static::lazy_static;
+use parking_lot::Mutex;
+
+const COMMAND_BUFFER_COUNT: usize = 3;
 
 pub static mut DEVICE_INITIALIZE_OFFS: *const u8 = 0 as _;
 pub static mut QUEUE_SUBMIT_COMMANDS_OFFS: *const u8 = 0 as _;
 pub static mut SET_TEXTURE_OFFS: *const u8 = 0 as _;
 pub static mut ACQUIRE_TEXTURE_OFFS: *const u8 = 0 as _;
-pub static mut DEVICE: *const nvn::Device = 0 as _;
 pub static mut TEXTURES: Vec<*mut nvn::Texture> = Vec::new();
 pub static mut TEXTURE_IDX: i32 = 0;
 
 
+lazy_static! {
+    static ref GLOBAL_MEMPOOL: Mutex<nvn::managed::MemPool> = {
+        let builder = nvn::managed::MemPool::new()
+            .with_device(nvn::global_device())
+            .with_flags(nvn::MemoryPoolFlags::new().with_cpu_uncached(true).with_gpu_cached(true))
+            .make_storage(0x1000 * COMMAND_BUFFER_COUNT, None);
+        Mutex::new(builder.finish().unwrap())
+    };
+
+    static ref COMMAND_BUFFERS: Mutex<Vec<nvn::managed::CommandBuffer>> = {
+        let mut vec = Vec::new();
+        let mut mempool = GLOBAL_MEMPOOL.lock();
+        for _ in 0..COMMAND_BUFFER_COUNT {
+            let cmd = mempool.reserve_mem(0x1000).unwrap().into_range();
+            vec.push(
+                nvn::managed::CommandBuffer::new()
+                    .with_device(nvn::global_device())
+                    .make_control(0x1000, None)
+                    .with_command(mempool.as_mut(), cmd.start, cmd.end - cmd.start)
+                    .finish()
+                    .unwrap()
+            );
+        }
+        Mutex::new(vec)
+    };
+}
 
 extern "C" {
     #[link_name = "\u{1}nvnBootstrapLoader"]
@@ -40,8 +68,8 @@ pub fn bootstrap_loader(func: *const u8) -> *const u8 {
 }
 
 #[hook(replace = DEVICE_INITIALIZE_OFFS)]
-pub fn device_initialize(device: *const nvn::Device, device_builder: *const nvn::DeviceBuilder) -> bool {
-    unsafe { DEVICE = device };
+pub fn device_initialize(device: *mut nvn::Device, device_builder: *const nvn::DeviceBuilder) -> bool {
+    nvn::set_global_device(device);
 
     let result = original!()(device, device_builder);
     
@@ -78,66 +106,35 @@ pub fn acquire_texture(window: *const u8, texture_available: *const u8, texture_
 #[hook(replace = QUEUE_SUBMIT_COMMANDS_OFFS)]
 pub fn queue_submit_commands(queue: &nvn::Queue, count: usize, handles: *mut nvn::CommandHandle) {
     if unsafe { TEXTURES.len() != 0} {
-        let device = unsafe { &*DEVICE };
         let texture = unsafe { TEXTURES[TEXTURE_IDX as usize] };
         let render_target: [*const nvn::Texture;1] = [texture];
 
         // Clone all the handles in a vector for ease of use
-        let mut command_handles = unsafe { std::slice::from_raw_parts_mut(handles, count) };
+        let command_handles = unsafe { std::slice::from_raw_parts(handles, count) };
         let mut new_handles: Vec<nvn::CommandHandle> = vec![];
         new_handles.extend_from_slice(command_handles);
 
-        // Create a MemoryPoolBuilder for our MemoryPool
-        let mut mem_builder = nvn::MemoryPoolBuilder::new();
-        let command_mem = unsafe { libc::memalign(4, 0x1000) } as _;
-
-        mem_builder.set_defaults();
-        mem_builder.set_device(device);
-        mem_builder.set_flags(nvn::MemoryPoolFlags::new().with_cpu_uncached(true).with_gpu_cached(true));
-        mem_builder.set_storage(command_mem, 0x1000);
-
-        let mut mem_pool = nvn::MemoryPool::new();
-        assert_eq!(mem_pool.initialize(&mem_builder), true, "Couldn't initialize MemoryPool");
-
-        // Create our CommandBuffer
-        let mut command_buf = nvn::CommandBuffer::new();    
-        // Initialize the CommandBuffer
-        assert_eq!(command_buf.initialize(device), true, "Couldn't initialize CommandBuffer");
-        let control_mem = unsafe { libc::memalign(8, 0x1000) } as _;
-        command_buf.add_command_memory(&mem_pool, 0, 0x1000);
-        command_buf.add_control_memory(control_mem, 0x1000);
-
-        let clear_color: &[f32;4] = &[1.0, 1.0, 0.0, 1.0];
-
-        // Start recording commands to scissor and clear an area of the screen
-        println!("Begin Recording for CommandBuffer");
-        command_buf.begin_recording();
-
-        command_buf.set_render_targets(1, &render_target as _, 0 as _, 0 as _, 0 as _);
-        command_buf.set_scissor(0, 0, 420, 420);
-        command_buf.set_viewport(0, 0, 420, 420);
-        // TODO: Fix ClearColorMask
-        command_buf.clear_color(0, clear_color as _, nvn::ClearColorMask::new().with_r(true).with_g(true).with_b(true));
-
-        // Stop recording, store our CommandHandle at the beginning of the vec
-        new_handles.insert(0, command_buf.end_recording());
-
-        // Call the original function with our new handle appended
-        let new_count = new_handles.len();
-        let new_handle_ptr = new_handles.leak().as_mut_ptr();
-        original!()(queue, new_count, new_handle_ptr as _);
-
-        // Cleanup
-        command_buf.finalize();
-
-        if command_mem != 0 as _ {
-            // Crashes for seemingly no reason. Probably some syncing issue?
-            mem_pool.finalize();
-            unsafe { libc::free(control_mem as _) };
+        if let Some(command_buf) = COMMAND_BUFFERS.lock().get_mut(unsafe { TEXTURE_IDX as usize }) {
+            command_buf.reset();
+            let clear_color: &[f32;4] = &[1.0, 1.0, 0.0, 1.0];
+    
+            // Start recording commands to scissor and clear an area of the screen
+            println!("Begin Recording for CommandBuffer");
+            command_buf.begin_recording();
+    
+            command_buf.set_render_targets(1, &render_target as _, 0 as _, 0 as _, 0 as _);
+            command_buf.set_scissor(0, 0, 420, 420);
+            command_buf.set_viewport(0, 0, 420, 420);
+            // TODO: Fix ClearColorMask
+            command_buf.clear_color(0, clear_color as _, nvn::ClearColorMask::new().with_r(true).with_g(true).with_b(true));
+    
+            // Stop recording, store our CommandHandle at the beginning of the vec
+            new_handles.insert(0, command_buf.end_recording());
+    
+            // Call the original function with our new handle appended
+            let new_count = new_handles.len();
+            original!()(queue, new_count, new_handles.as_ptr() as _);
         }
-        
-        unsafe { libc::free(command_mem as _) };
-        unsafe { libc::free(new_handle_ptr as _) };
 
     }
     else {
